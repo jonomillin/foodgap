c7c4c7e968538f6be2bff4a0acce0224a2b36be0
Nicholas Pilkington <nicholas.pilkington@gmail.com>
2013-04-14 18:16:56 -0700

    Adding login page

--- js/my.js
+++ js/my.js
@@ -1,13 +1,36 @@
-function init() {
- document.addEventListener("deviceready", deviceReady, true);
- delete init;
-}
-
 var dealAPI = 'http://secret-coast-5576.herokuapp.com/deals';
 var confirmAPI = 'http://secret-coast-5576.herokuapp.com/confirm';
+var signinAPI = 'http://secret-coast-5576.herokuapp.com/api/signin';
+
+// var dealAPI = 'http://127.0.0.1:10080/deals';
+// var confirmAPI = 'http://127.0.0.1:10080/confirm';
+// var signinAPI = 'http://127.0.0.1:10080/api/signin';
+
 var cache;
 var useCaching = false;
 // Make httpPost request to url and then return callback.
+
+function init() {
+    console.log('init called');
+    document.addEventListener("deviceready", deviceReady, true);
+    delete init;
+}
+
+
+function checkPreAuth() {
+    console.log('Int pre auth');
+    var form = $("#loginForm");
+    if(window.localStorage["email"] != undefined && window.localStorage["password"] != undefined) {
+        $("#email", form).val(window.localStorage["email"]);
+        $("#password", form).val(window.localStorage["password"]);
+        handleLogin();
+    }
+    else
+    {
+        console.log('NOthing stored');
+    }
+}
+
 function handleLogin() {
     var form = $("#loginForm");    
     //disable the button so we can't resubmit while we wait
@@ -15,40 +38,39 @@ function handleLogin() {
     var u = $("#email", form).val();
     var p = $("#password", form).val();
     console.log("click");
+    console.log(u);
+    console.log(p);
     if(u != '' && p!= '') {
-        $.post("http://secret-coast-5576.herokuapp.com", {email:u,password:p}, function(res) {
-            if(res == true) {
-                //store
-                window.localStorage["email"] = u;
+        $.post(signinAPI, {email:u,password:p}, function(res) {
+            console.log(res);
+            if(res.status == "ok") {
+               window.localStorage["email"] = u;
                window.localStorage["password"] = p;             
-                $.mobile.changePage("some.html");
+               console.log('success');
+                $.mobile.changePage("main.html");
             } else {
-                navigator.notification.alert("Your login failed", function() {});
+                alert("Your login failed");
             }
          $("#submitButton").removeAttr("disabled");
         },"json");
+    } else {
+        //Thanks Igor!
+        alert("You must enter a email and password");
+        $("#submitButton").removeAttr("disabled");
     }
     return false;
 }
 
-alert('here');
+function deviceReady() {
+ console.log('device reading called');
+ $("#loginForm").on("submit",handleLogin);
 
-function checkPreAuth() {
-    var form = $("#loginForm");
-    if(window.localStorage["email"] != undefined && window.localStorage["password"] != undefined) {
-        $("#email", form).val(window.localStorage["email"]);
-        $("#password", form).val(window.localStorage["password"]);
-        handleLogin();
-    }
 }
-
-$("#loginPage").live("pageinit", function(e) {
-       checkPreAuth();
-   });
 
 function httpPost (url, jdata, callback)
 {
-  $.post(url, jdata, callback);
+
+  $.post(url, jdata, callback).error( function() { alert("We're making some updates at our end. Check back in a little while!")});
 }
 
 // Some filthy hack to pass parameters between pages.
@@ -76,9 +98,9 @@ function httpGet( url, callback)
                 cache = data;
             }
             return callback(data);
-        });
+        }).error( function() { alert("We're making some updates at our end. Check back in a little while!")});
     } else {
-        $.getJSON(url, callback);
+        $.getJSON(url, callback).error( function() { alert("We're making some updates at our end. Check back in a little while!")});
     }
 }
 
@@ -86,7 +108,7 @@ function build_out_element(item, name, image, _id)
 {
     var u = 'url("'+image+'")';    
     var el = $("<a>", 
-    {"href":"meal_choice.html?choice=" + _id, "data-transition":"slide", "class":"box restaurant", "id":"Rest"+item})
+    {"href":"meal_choice.html?choice=" + _id, "data-transition":"slide", "class":"box restaurant rest"})
     .css('background-image', u)
     .html('<p id="Rest' + item
         + 'Name">'+name+'<div class="yelp star5"></div></p>');
@@ -100,18 +122,19 @@ function build_meal_choice_element(item, image, name, price, description)
         
     }
     var el = $("<div>", 
-    {"data-transition":'slide', "class" :'box meal', "id" :'Meal'+item })
+    {"data-transition":'slide', "class" :'box meal' })
     .css('background-image', 'url("'+image+'")')
     .html('<div data-role="fieldcontain" class="top-layer">'
            +'<a href="#" class="add-circle" '
             + 'onclick="javascript:$(\'#quantity_'+item+'\').val(parseInt($(\'#quantity_'+item+'\').val())+1);">'
-             + '<div class="plus">afds</div></a>'
-           +'<input type="" class="quantity" min="0" max="10" name="quantity_'+item+'" id="quantity_'+item+'" value="0" />'
+            + '<div class="plus"></div></a>'
+           +'<input type="" readonly class="quantity" min="0" max="10" name="quantity_'+item+'" id="quantity_'+item+'" value="0" />'
            +'<a href="#" class="add-circle"'
-            + 'onclick="javascript:$(\'#quantity_'+item+'\').val(parseInt($(\'#quantity_'+item+'\').val())-1);"> <div class="minus"></div></a>'
+            + 'onclick="javascript:$(\'#quantity_'+item+'\').val(parseInt(Math.max(0, $(\'#quantity_'+item+'\').val()-1)));">'
+            + '<div class="minus"></div></a>'
            + '</div>'
            + '<div class=desc> <span id="dish_name"> ' + name 
-           +'</span> <span id="dish_price">$' + price/100
+           +'</span> <span id="dish_price">$' + parseFloat(price/100).toFixed(2)
            +'</span> <br/> <small id="dish_desc">' + description
            +'</small></div> '); 
     return el;
@@ -129,41 +152,49 @@ function build_meal_choice_element(item, image, name, price, description)
 
 // Fetch and show all the options for eating out at a particular restaurant. 
 $('#out').live('pageshow', function () {     
+    $.mobile.showPageLoadingMsg();
+    var dinner_type = $.urlParam('dinner_type');
+    //alert(dinner_type);
+    console.log(dinner_type);
     // Fetch all of todays deals
     httpGet(dealAPI, function(f) { 
         // Clear out the div
         // console.log('Received length: ' + f.length);
         // console.log('Clearing the div.');
-        $('#out_choices').html('');   
+        //$('#out_choices').html('');   
         //Loop through the deal
+        $.mobile.hidePageLoadingMsg();
         $.each(f, function (i, data) {
-            // console.log('Received: ' + data.name);
+            //console.log('Received: ')
+            //console.log(data);
             // Only restaurants
-            if (data['dinner_type'] === 'Eat out') {
+            if (data['dinner_type'] == dinner_type) {
                 // console.log('Adding: ' + data.name);
-                
+                console.log('Showing: ' + data.name);   
                 el = build_out_element(i, data.name, data.image, data["_id"]["$oid"]);
                 // console.log('Adding');
                 $('#out_choices').append(el);
             }
             else
             {
-                console.log('NOT adding non-restaurant: ' + data.name);
+                console.log('Not showing: ' + data.name);
             }
         });  
+    
     });
 });
 
 $('#meal_choice').live('pageshow', function () {
-
+    $.mobile.showPageLoadingMsg();
     var choice = $.urlParam('choice');
 
     httpGet(dealAPI, function(dinners) { 
 
+
     $('#meal_choice_choices').html('');
   
     for(var k = 0; k < dinners.length; k++)
-{
+    {
 
         if (dinners[k]["_id"]["$oid"] != choice) continue;
     $.each(dinners[k].components, function (i, data) {
@@ -171,17 +202,28 @@ $('#meal_choice').live('pageshow', function () {
         $('#meal_choice_choices').append(el);
     });  
         }
+    $.mobile.hidePageLoadingMsg();
     });
 
     $('#order').click(function() {
         var postData = {}
-        console.log('In quantities.');
+        var totalItems = 0;
          $('input[name^="quantity"]').each( function() {
             postData[this.name] = this.value;
+            totalItems += this.value;
             console.log(this.name);
          });
+
+         if (totalItems == 0)
+         {
+            alert('You must select at least on item');
+            $('#order').removeClass('ui-btn-active')
+            return 0;
+         }
         
         postData["dinner_id"] = choice;
+        postData["email"] = window.localStorage["email"];
+
         httpPost(dealAPI, postData, function(data)
         {
             console.log("Back from confirmations.");
@@ -192,47 +234,48 @@ $('#meal_choice').live('pageshow', function () {
         });
     });
 
-
 });
 
 
 
 
 $('#confirm').live('pageshow', function () {     
-
+    $.mobile.showPageLoadingMsg();
     var order_id = $.urlParam('order_id');
     
     httpGet(confirmAPI + "/" + order_id, function( f ) { 
        
+       console.log('Final order');
+       console.log(f);
+
+               
+        var k = 0;
+        for(var item in f['dinner']['components'])
+        {
+            var obj = f['dinner']['components'][item];
+            console.log(obj['name'] + ' - ' + obj['quantity'] + 'x' + obj['price']);
+            $('#item' + k).html(obj['quantity'] + ' x ' + obj['name']);
+            $('#price' + k).html('$' + parseFloat( parseInt(obj['quantity']) * parseInt(obj['price']) / 100.0).toFixed(2));
+            k = k + 1;
+        }
+
+       $('#total_price').html('$' + parseFloat(f['price'] / 100).toFixed(2));
+       $('#address').html(f['dinner']['name']);
+       $('#contact_info').html(window.localStorage["email"]);
         // Set page items from json object.
 
         $('#confirm_button').bind('click', function() {
             console.log("Clicked confirm");
             
-            httpPost (confirmAPI, {"order_id" : order_id}, function() {
+            httpPost (confirmAPI, {"order_id" : order_id, 'time' : $('#booking_time').val()}, function() {
                 $.mobile.changePage( "success.html", { transition: "slide" });
 
             } );
 
         });
+    $.mobile.hidePageLoadingMsg();
     });
 
 });
 
-function deviceReady() {
-    
- $("#loginForm").on("submit",handleLogin);
 
-}
-
-
-
-
-// try {
-//     $(function() { 
-//         alert('running');
-//         app();
-//     });
-// } catch (error) {
-//     console.error("Your javascript has an error: " + error);
-// }
